3511e540a12df6795f550910734d344939d69275
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-01-14 17:30:28 +0000

    Add files via upload
    Updates.

--- orbital.hpp
+++ orbital.hpp
@@ -18,8 +18,19 @@ class orbital{
 
 private:
     std::string m_tle_name;
-    std::string m_satellite_number;
+    int m_satellite_number;
     std::string m_classification;
+    int         m_designator_launch_year;
+    int         m_designator_launch_number_of_year;
+    char        m_designator_piece_of_launch;
+    int         m_epoch_year;
+    double      m_julian_date_fraction;
+    double      m_ballistic_coefficient;
+    double      m_second_derivative_of_motion;
+    std::string m_drag_term_or_radition_coefficient;
+    int         m_ephemeris_type;
+    int         m_element_number;
+    int         m_check_sum;
     
     int m_year;
     int m_month;
@@ -28,9 +39,37 @@ private:
 public:
     orbital(int year, int month, int day);
     void getDataFile();
-    void SetTLEname(std::string name);
     void SetDate(int year, int month, int day);
-    std::string getTLEname();
+    void SetTLEname(std::string name);
+    void SetSATnumber(int number);
+    void SetCLASSIFIERfield(std::string field);
+    void SetLAUNCHERyearfield(int field);
+    void SetLAUNCHERnumberfield(int field);
+    void SetLAUNCHpiencedesignator(char designator);
+    void SetEPOCHyear(int year);
+    void SetJULIANdatefraction(double fraction);
+    void SetBALLISTICcoefficient(double coefficient);
+    void SetSECONDderivativemotion(double motion);
+    void SetDRAGRADIATIONcoefficient(int coefficient);
+    void SetEPHEMERIStype(int type);
+    void SetELEMENTnumber(int number);
+    void SetCHECKsum(int sum);
+    
+    std::string getTLEname() { return m_tle_name; }
+    int getSATnumber() { return m_satellite_number; }
+    std::string getCLASSIFIERfield() { return m_classification; }
+    int getLAUNCHERyearfield() { return m_classification; }
+    int getLAUNCHERnumberfield();
+    char getLAUNCHpiencedesignator();
+    int getEPOCHyear();
+    double getJULIANdatefraction();
+    double getBALLISTICcoefficient();
+    double getSECONDderivativemotion();
+    int getDRAGRADIATIONcoefficient();
+    int getEPHEMERIStype();
+    int getELEMENTnumber();
+    int getCHECKsum();
+    
     int getYear() { return m_year; }
     int getMonth() { return m_month; }
     int getDay()  { return m_day; }
