cd20546c950095cfdb6bbf6faab748ccc2e0d4ad
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-01-20 08:31:26 +0000

    Add files via upload
    Updates to files.

--- main.cpp
+++ main.cpp
@@ -6,21 +6,19 @@
 //  Copyright © 2020 Kwadwo Oteng-Amoko. All rights reserved.
 //
 
+ 
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include "orbital.hpp"
 
-int main() {
-    // insert code here...
+int main()
+{
+    // main file with some dummy code
     std::cout << "Hello, Kwadwo!\n";
     std::vector<int> v{1,2,3};
     orbital *orb = new orbital(12,13,14);
     orb->getDataFile();
-    std::cout << "\nsatellite name: " << orb->getTLEname() << "\n";
+    std::cout << "\nsatellite name: " << orb -> getTLEname() << "\n";
     return 0;
-    
-    
- 
-    
 }
