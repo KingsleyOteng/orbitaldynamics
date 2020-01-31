e3e32d3e82b3c774c857ce7e6b8db81a10e9b917
Kwadwo Oteng-Amoako <k.oteng@ieee.org>
2020-01-31 18:11:43 +0000

    Sensor implementation

---
+++ source/sensor.cpp
@@ -0,0 +1,9 @@
+//
+//  sensor.cpp
+//  source
+//
+//  Created by Kwadwo Oteng-Amoko on 31/01/2020.
+//  Copyright © 2020 Kwadwo Oteng-Amoko. All rights reserved.
+//
+
+#include "sensor.hpp"
---
+++ source/sensor.hpp
@@ -0,0 +1,28 @@
+//
+//  sensor.hpp
+//  source
+//
+//  Created by Kwadwo Oteng-Amoko on 31/01/2020.
+//  Copyright © 2020 Kwadwo Oteng-Amoko. All rights reserved.
+//
+
+#ifndef sensor_hpp
+#define sensor_hpp
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+
+class sensor{
+    
+
+private:
+    
+    
+public:
+    
+};
+
+
+#endif /* orbital_hpp */
