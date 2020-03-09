7d39677589b8a02afcc541419864f082c7a8f5af
Kwadwo Oteng-Amoako <k.oteng@ieee.org>
2020-03-09 10:00:45 +0000

    New sun model and geomagnetic field model.

---
+++ geomagnetic_field_model.hpp
@@ -0,0 +1,14 @@
+//
+//  geomagnetic_field_model.hpp
+//  source
+//
+//  Created by Kwadwo Oteng-Amoko on 09/03/2020.
+//  Copyright © 2020 Kwadwo Oteng-Amoko. All rights reserved.
+//
+
+#ifndef geomagnetic_field_model_hpp
+#define geomagnetic_field_model_hpp
+
+#include <stdio.h>
+
+#endif /* geomagnetic_field_model_hpp */
---
+++ sun_model.hpp
@@ -0,0 +1,14 @@
+//
+//  sun_model.hpp
+//  source
+//
+//  Created by Kwadwo Oteng-Amoko on 09/03/2020.
+//  Copyright © 2020 Kwadwo Oteng-Amoko. All rights reserved.
+//
+
+#ifndef sun_model_hpp
+#define sun_model_hpp
+
+#include <stdio.h>
+
+#endif /* sun_model_hpp */
