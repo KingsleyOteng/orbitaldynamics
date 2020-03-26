37631dbdf5ac1b6fa0eb54dc0e45dd80e63f6944
Kwadwo Oteng-Amoako <k.oteng@ieee.org>
2020-03-26 09:26:56 +0000

    Updates

---
+++ source/BenchMarking.hpp
@@ -0,0 +1,35 @@
+//
+//  BenchMarking.hpp
+//  source
+//
+//  Created by Kwadwo Oteng-Amoko on 26/03/2020.
+//  Copyright © 2020 Kwadwo Oteng-Amoko. All rights reserved.
+//
+
+#ifndef BenchMarking_hpp
+#define BenchMarking_hpp
+
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <ctime>
+#include <string>
+#include <iostream>
+#include "coordinate_transforms.hpp"   // coordinate transforms library
+
+using namespace std;
+//
+
+class benchmarking
+{
+   
+    
+private:
+    
+    
+public:
+      void getBenchMarking                        ();
+    
+};
+
+#endif /* BenchMarking_hpp */
