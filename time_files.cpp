e40992d71633e0e9850366366818bc97ea26e34e
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-03-31 17:30:36 +0000

    Add files via upload
    Updates.

---
+++ time_files.cpp
@@ -0,0 +1,17 @@
+//
+//  time_files.cpp
+//  source
+//
+//  Created by Kwadwo Oteng-Amoko on 28/03/2020.
+//  Copyright © 2020 Kwadwo Oteng-Amoko. All rights reserved.
+//
+
+#include "time_files.hpp"
+
+std::tm*
+time_files::jdTOtm      (double jd)
+{
+    std::tm*                    convertJDtoTM;
+    
+    return convertJDtoTM;
+}
