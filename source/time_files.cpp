3c5efd9682dc63bd3ce6aa6dcfd46499f24a65c0
Kwadwo Oteng-Amoako <k.oteng@ieee.org>
2020-04-04 03:13:24 +0000

    Updates.

--- source/time_files.cpp
+++ source/time_files.cpp
@@ -152,30 +152,57 @@ time_files::jdTOctime     (double jd)
 double
 time_files::ctimeTOjd     (char* ctime)
 {
-
+    std::string phrase = ctime;
     
-     std::string hello = ctime;
-    
-    
-    
-    int fract = 1;
-    int month = 1;
-
-    int day_int;
-    int hour_int;
-    int min_int;
-    int sec_int;
-    
-    std::string phrase = "Fri Apr  3 22:26:58 2020";
+    std::vector<std::string> months = {"JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"};
+    size_t month_number = std::distance(months.begin(),std::find(months.begin(), months.end(), "MAR"));
+    cout << "index" << index << "\n";
     
     boost::xpressive::sregex rex = boost::xpressive::sregex::compile( "(\\w+) (\\w+)  (\\d+) (\\d+):(\\d+):(\\d+) (\\d+)" );
     boost::xpressive::smatch what;
     
+    int temp;
+    //temp = what[3];
+    
+    
+    
+   // int hour_num = int.Parse(what[4]);
+   // int minute_num = int(what[5]);
+   // int sec_num = int(what[6]);
+   // int year_num = int(what[7]);
+    
     if( regex_match( phrase, what, rex ) )
     {
+        
+        int hour_num = stoi(what[4].str());
+        int minute_num = stoi(what[5].str());
+        int sec_num = stoi(what[6].str());
+        int year_num = stoi(what[7].str());
+        int day_num = stoi(what[3].str());
+        
+        cout << "yy" << hour_num << "\n";
+        
         cout << "what[0]" << what[0] << "\n";
-    }cout << "what[1]" << what[1] << "\n";
-    cout << "what[2]" << what[2] << "\n";
-    
+        cout << "what[1]" << what[1] << "\n";
+        cout << "what[2]" << month_number << "\n";
+        cout << "what[3]" << what[3] << "\n";
+        cout << "what[4]" << what[4] << "\n";
+        cout << "what[5]" << what[5] << "\n";
+        cout << "what[6]" << what[6] << "\n";
+         cout << "what[7]" << what[7] << "\n";
+        
+       
+        
+        
+        
+        //std::tm* tm = {0};
+        //tm->tm_sec = sec_num;
+        //tm->tm_min = minute_num;
+        //tm->tm_hour = hour_num;
+        //tm->tm_mday = day_num;
+        //tm->tm_mon = int(month_number);
+       // tm->tm_year = year_num;
+        //tm->tm_isdst = 0;
+    }
     return 0;
 }
