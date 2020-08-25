b00e765cd0a7fa00e17fa31a33f058cde392f159
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-08-25 16:20:25 +0000

    Add files via upload
    Updates.

--- timetablegeneratormodel/FXMLDocumentController.java
+++ timetablegeneratormodel/FXMLDocumentController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package timetablegeneratormodel;
 
 import java.net.URL;
@@ -13,7 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 /**
- *
+ * @details text file generator from ODBC
  * @author kwadwooteng-amoko
  */
 public class FXMLDocumentController implements Initializable {
