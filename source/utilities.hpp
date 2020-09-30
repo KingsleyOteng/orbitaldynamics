2bd94e1d7417efd7a4164b3cc0a6b16e861e000e
Kwadwo Oteng-Amoako <k.oteng@ieee.org>
2020-09-30 22:30:44 +0000

    Read specific fields from the satellite database

--- source/utilities.hpp
+++ source/utilities.hpp
@@ -71,6 +71,6 @@ public:
   char*                     GetSimpleDateFormatLong          ();
   double                    GetEpochJulianDate               ();
   std::string               GetSatelliteLog                  (std::string phrasalmatching);
-  void                      SatelliteNORADRecord             (std::string noradId);
+  void                      SatelliteNORADRecord             (std::string noradId, int parameter);
 };
 #endif /* utilities_hpp */
