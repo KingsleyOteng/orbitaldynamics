e5ab5e4959c5882b6f180a7f277461cac5b3891e
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-10-11 19:49:08 +0000

    Add files via upload
    Updates to the code.

--- sgp4.hpp
+++ sgp4.hpp
@@ -6,12 +6,31 @@
 //  Copyright © 2020 Kwadwo Oteng-Amoko. All rights reserved.
 //
 
+#include <stdio.h>
+#include <string>
+#include <sstream>      // std::istringstream
+#include <iostream>     // std::cout
+#include <vector>
+#include "orbital.hpp"
+#include "utilities.hpp"
+
 #ifndef sgp4_hpp
 #define sgp4_hpp
+class sgp4
+{
+    
+    private:
+    
+    orbital *orb = new orbital();
+    utilities *util = new utilities();
+    
+    public:
 
-#include <stdio.h>
+     // constructors  and destructors
+     sgp4();
+    ~sgp4();
+};
 
 #endif /* sgp4_hpp */
 
-orbital *orb = new orbital();
-utilities *util = new utilities();
+
