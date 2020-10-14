4b24a7c4d9aa2d796a01915266dd056e5deb6967
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-10-14 20:59:32 +0000

    Add files via upload
    Updates.

--- sgp4.cpp
+++ sgp4.cpp
@@ -55,12 +55,12 @@ void builds()
     int         m_element_number = orb -> getELEMENTnumber();
     int         m_check_sum;
     int         m_satellite_number_2 = orb -> getSATNUMBERline2();
-    double      m_satellite_inclination;
-    double      m_satellite_ascension_node;
-    double      m_satellite_eccentricity_coefficient;
-    double      m_argument_perigree;
-    double      m_mean_anomaly;
-    double      m_mean_motion;
+    double      m_satellite_inclination = orb -> getINCLINATIONfield();
+    double      m_satellite_ascension_node = orb -> getRIGHTASCENTIONnode();
+    double      m_satellite_eccentricity_coefficient = orb->getECCENTRICITYcoefficient();
+    double      m_argument_perigree = orb->getARGUMENTperigree();
+    double      m_mean_anomaly = orb -> getMEANanomlay();
+    double      m_mean_motion = orb -> getMEANmotion();
     double      m_epoch_checksum = orb -> getREVOLUTIONepochchecksum(); //???
     int         m_lineone_checksum = orb -> getCheckSumLineOne();
     int         m_linetwo_checksum = orb -> getCheckSumLineTwo();
