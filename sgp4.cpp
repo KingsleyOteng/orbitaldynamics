863cdb62a53b17eb57f22a09853532ab8715ea45
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-10-19 08:39:05 +0000

    Add files via upload
    Clean up of sgp4 implementation.

--- sgp4.cpp
+++ sgp4.cpp
@@ -41,33 +41,30 @@ sgp4::sgp4                            ()
             orb -> SetTLEparameters (orb -> getTLEname(), parsed_line_ones, parsed_line_twos);
     
     // member variables
-             m_satellite_number = orb -> getSATnumber();
- 
-                strcpy(m_classification,orb->getCLASSIFIERfield());
-             m_designator_launch_year = orb -> getLAUNCHERyearfield(); // ??
-             m_designator_launch_number_of_year = orb -> getLAUNCHERnumberfield(); //??
-     
-                strcpy(m_designator_piece_of_launch,orb -> getLAUNCHpiencedesignator());
-             m_epoch_year = orb -> getEPOCHyear();
+          strcpy(m_designator_piece_of_launch,orb -> getLAUNCHpiencedesignator());
+          strcpy(m_second_derivative_of_motion,orb -> getSECONDderivativemotion());
+          strcpy(m_second_derivative_of_motion,orb -> getSECONDderivativemotion());
+          strcpy(m_classification,orb->getCLASSIFIERfield());
+    
+          m_satellite_number = orb -> getSATnumber();
+          m_designator_launch_year = orb -> getLAUNCHERyearfield(); // ??
+          m_designator_launch_number_of_year = orb -> getLAUNCHERnumberfield(); //??
+          m_epoch_year = orb -> getEPOCHyear();
           m_julian_date_fraction = orb -> getJULIANdatefraction();
           m_ballistic_coefficient = orb -> getBALLISTICcoefficient();
-
-                strcpy(m_second_derivative_of_motion,orb -> getSECONDderivativemotion());
-
-                strcpy(m_second_derivative_of_motion,orb -> getSECONDderivativemotion());
-             m_ephemeris_type = orb -> getEPHEMERIStype();
-             m_element_number = orb -> getELEMENTnumber();
-             m_check_sum = orb -> getCheckSumLineOne(); //????
-             m_satellite_number_2 = orb -> getSATNUMBERline2();
+          m_ephemeris_type = orb -> getEPHEMERIStype();
+          m_element_number = orb -> getELEMENTnumber();
+          m_check_sum = orb -> getCheckSumLineOne(); //????
+          m_satellite_number_2 = orb -> getSATNUMBERline2();
           m_satellite_inclination = orb -> getINCLINATIONfield();
           m_satellite_ascension_node = orb -> getRIGHTASCENTIONnode();
           m_satellite_eccentricity_coefficient = orb->getECCENTRICITYcoefficient();
           m_argument_perigree = orb->getARGUMENTperigree();
           m_mean_anomaly = orb -> getMEANanomlay();
           m_mean_motion = orb -> getMEANmotion();
-          m_epoch_checksum = orb -> getREVOLUTIONepochchecksum(); //???
-             m_lineone_checksum = orb -> getCheckSumLineOne();
-             m_linetwo_checksum = orb -> getCheckSumLineTwo();
+          m_epoch_checksum = orb -> getREVOLUTIONepochchecksum();
+          m_lineone_checksum = orb -> getCheckSumLineOne();
+          m_linetwo_checksum = orb -> getCheckSumLineTwo();
 }
 
 // destructor
