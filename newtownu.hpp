50928ad7bafc9bd044f2268dd743974d2f5e70de
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-11-28 09:00:20 +0000

    Add files via upload
    Updates to cpp

---
+++ newtownu.hpp
@@ -0,0 +1,43 @@
+//
+//  newtownu.hpp
+//  source
+//
+//  Created by Kwadwo Oteng-Amoako on 28/11/2020.
+//  Copyright © 2020 Kwadwo Oteng-Amoko. All rights reserved.
+//
+
+#ifndef newtownu_hpp
+#define newtownu_hpp
+
+
+#include <stdio.h>
+#include <string>
+#include <sstream>      // std::istringstream
+#include <iostream>     // std::cout
+#include <vector>
+
+
+class newtownu
+{
+    
+private:
+    
+   // macro defining array size
+   #define         numiter                              50
+   #define         small                      256
+
+    
+  // members
+  double halfpi;
+  double ecc;
+  double m;
+  double e0;
+  double e1;
+  double ktr;
+
+    
+void setNewtonU
+    (double ecc, double m);
+  
+};
+#endif /* utilities_hpp */
