17cb8f949c31a9badc1dfd9caf0ad62da5e98e49
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-11-29 17:10:20 +0000

    Add files via upload
    Updates.

--- newtownu.cpp
+++ newtownu.cpp
@@ -8,62 +8,62 @@
 
 
 #include "newtownu.hpp"
-//% ------------------------------------------------------------------------------
-//%
-//%                           function newtonm
-//%
-//%  this function performs the newton rhapson iteration to find the
-//%    eccentric anomaly given the mean anomaly.  the true anomaly is also
-//%    calculated.
-//%
-//%  author        : david vallado                  719-573-2600    9 jun 2002
-//%
-//%  revisions
-//%                -
-//%
-//%  inputs          description                    range / units
-//%    ecc         - eccentricity                   0.0  to
-//%    m           - mean anomaly                   -2pi to 2pi rad
-//%
-//%  outputs       :
-//%    e0          - eccentric anomaly              0.0  to 2pi rad
-//%    nu          - true anomaly                   0.0  to 2pi rad
-//%
-//%  locals        :
-//%    e1          - eccentric anomaly, next value  rad
-//%    sinv        - sine of nu
-//%    cosv        - cosine of nu
-//%    ktr         - index
-//%    r1r         - cubic roots - 1 to 3
-//%    r1i         - imaginary component
-//%    r2r         -
-//%    r2i         -
-//%    r3r         -
-//%    r3i         -
-//%    s           - variables for parabolic solution
-//%    w           - variables for parabolic solution
-//%
-//%  coupling      :
-//%    cubic       - solves a cubic polynomial
-//%
-//%  references    :
-//%    vallado       2001, 72-75, alg 2, ex 2-1
-//%
-//% [e0,nu] = newtonm ( ecc,m );
-//% ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+//
+//                           function newtonm
+//
+//  this function performs the newton rhapson iteration to find the
+//    eccentric anomaly given the mean anomaly.  the true anomaly is also
+//    calculated.
+//
+//  author        : david vallado                  719-573-2600    9 jun 2002
+//
+//  revisions
+//                -
+//
+//  inputs          description                    range / units
+//    ecc         - eccentricity                   0.0  to
+//    m           - mean anomaly                   -2pi to 2pi rad
+//
+//  outputs       :
+//    e0          - eccentric anomaly              0.0  to 2pi rad
+//    nu          - true anomaly                   0.0  to 2pi rad
+//
+//  locals        :
+//    e1          - eccentric anomaly, next value  rad
+//    sinv        - sine of nu
+//    cosv        - cosine of nu
+//    ktr         - index
+//    r1r         - cubic roots - 1 to 3
+//    r1i         - imaginary component
+//    r2r         -
+//   r2i         -
+//    r3r         -
+//    r3i         -
+//    s           - variables for parabolic solution
+//    w           - variables for parabolic solution
+//
+// coupling      :
+//    cubic       - solves a cubic polynomial
+//
+//  references    :
+//    vallado       2001, 72-75, alg 2, ex 2-1
+//
+// [e0,nu] = newtonm ( ecc,m );
+//------------------------------------------------------------------------------
 
 void newtownu::setNewtonU
  (double ecc, double m)
 {
     
     
-    //% -------------------------  implementation   -----------------
+    // -------------------------  implementation   -----------------
     halfpi  = pi * 0.5;
     
-    // % -------------------------- hyperbolic  ----------------------
+    // -------------------------- hyperbolic  ----------------------
     if ( (ecc-1.0 ) > small )
     {
-        //% -------------------  initial guess -----------------------
+        // -------------------  initial guess -----------------------
         if ( ecc < 1.6  )
         {
             if ( ((m<0.0 ) & (m>-pi)) | (m>pi) )
@@ -101,14 +101,14 @@ void newtownu::setNewtonU
             e1= e0 + ( ( m - ecc*sinh(e0) + e0 ) / ( ecc*cosh(e0) - 1.0  ) );
             ktr = ktr + 1;
         }
-        // % ----------------  find true anomaly  --------------------
+        // ----------------  find true anomaly  --------------------
         sinv = -( sqrt( ecc*ecc-1.0  ) * sinh(e1) ) / ( 1.0  - ecc*cosh(e1) );
         cosv = ( cosh(e1) - ecc ) / ( 1.0  - ecc*cosh(e1) );
         nu   = atan2( sinv,cosv );
     }
     else
     {
-        //  % --------------------- parabolic -------------------------
+        // --------------------- parabolic -------------------------
         if ( abs( ecc-1.0  ) < small )
         {
             s = 0.5  * (halfpi - atan( 1.5 *m ) );
@@ -119,10 +119,10 @@ void newtownu::setNewtonU
         }
         else
         {
-            //     % -------------------- elliptical ----------------------
+            // -------------------- elliptical ----------------------
             if ( ecc > small )
             {
-                //        % -----------  initial guess -------------
+                // -----------  initial guess -------------
                 if ( ((m < 0.0 ) & (m > -pi)) | (m > pi) )
                 {
                     e0= m - ecc;
@@ -139,14 +139,14 @@ void newtownu::setNewtonU
                     e0= e1;
                     e1= e0 + ( m - e0 + ecc*sin(e0) ) / ( 1.0  - ecc*cos(e0) );
                 }
-                //        % -------------  find true anomaly  ---------------
+                // -------------  find true anomaly  ---------------
                 sinv= ( sqrt( 1.0 -ecc*ecc ) * sin(e1) ) / ( 1.0 -ecc*cos(e1) );
                 cosv= ( cos(e1)-ecc ) / ( 1.0  - ecc*cos(e1) );
                 nu  = atan2( sinv,cosv );
             }
             else
             {
-                //         % -------------------- circular -------------------
+                // -------------------- circular -------------------
                 ktr= 0;
                 nu= m;
                 e0= m;
