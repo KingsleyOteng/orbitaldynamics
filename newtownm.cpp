7484ef108f79b9feaa825b0f695e53ff5bae3978
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-11-29 17:20:26 +0000

    Add files via upload
    Updates.

--- newtownm.cpp
+++ newtownm.cpp
@@ -53,76 +53,89 @@
 // [e0,nu] = newtonm ( ecc,m );
 // ------------------------------------------------------------------------------
 
-function [e0,nu] = newtonm ( ecc,m );
-
-        % -------------------------  implementation   -----------------
-        numiter =    50;
-        small   =     0.00000001;
-        halfpi  = pi * 0.5;
+void newtownM::setNewtonM
+(double ecc, double m)
+{
+    
+    
+    //-------------------------  implementation   -----------------
+    numiter =    50;
+    small   =     0.00000001;
+    halfpi  = pi * 0.5;
+    
+    // -------------------------- hyperbolic  ----------------------
+    if ( (ecc-1.0 ) > small )
+    {
         
-        % -------------------------- hyperbolic  ----------------------
-        if ( (ecc-1.0 ) > small )
-           % -------------------  initial guess -----------------------
-            if ( ecc < 1.6  )
-                if ( ((m<0.0 ) & (m>-pi)) | (m>pi) )
-                    e0= m - ecc;
-                  else
-                    e0= m + ecc;
-                  end
-              else
+        // -------------------  initial guess -----------------------
+        if ( ecc < 1.6  )
+            if ( ((m<0.0 ) & (m>-pi)) | (m>pi) )
+            {
+                e0= m - ecc;
+            }
+            else
+            {
+                e0= m + ecc;
+            }
+            else
+            {
                 if ( (ecc < 3.6 ) & (abs(m) > pi) )
+                {
                     e0= m - sign(m)*ecc;
-                  else
+                }
+                else
+                {
                     e0= m/(ecc-1.0 );
-                  end
-              end
-            ktr= 1;
-            e1 = e0 + ( (m-ecc*sinh(e0)+e0) / (ecc*cosh(e0) - 1.0 ) );
-            while ((abs(e1-e0)>small ) & ( ktr<=numiter ))
-                e0= e1;
-                e1= e0 + ( ( m - ecc*sinh(e0) + e0 ) / ( ecc*cosh(e0) - 1.0  ) );
-                ktr = ktr + 1;
-              end
-            % ----------------  find true anomaly  --------------------
-            sinv= -( sqrt( ecc*ecc-1.0  ) * sinh(e1) ) / ( 1.0  - ecc*cosh(e1) );
-            cosv= ( cosh(e1) - ecc ) / ( 1.0  - ecc*cosh(e1) );
-            nu  = atan2( sinv,cosv );
-          else
-            % --------------------- parabolic -------------------------
-            if ( abs( ecc-1.0  ) < small )
-%                c = [ 1.0/3.0; 0.0; 1.0; -m];
-%                [r1r] = roots (c);
-%                e0= r1r;
-                 s = 0.5  * (halfpi - atan( 1.5 *m ) );
-                 w = atan( tan( s )^(1.0 /3.0 ) );
-                 e0= 2.0 *cot(2.0 *w);
-                ktr= 1;
-                nu = 2.0  * atan(e0);
-              else
-                % -------------------- elliptical ----------------------
-                if ( ecc > small )
-                    % -----------  initial guess -------------
-                    if ( ((m < 0.0 ) & (m > -pi)) | (m > pi) )
-                        e0= m - ecc;
-                      else
-                        e0= m + ecc;
-                      end
-                    ktr= 1;
-                    e1 = e0 + ( m - e0 + ecc*sin(e0) ) / ( 1.0  - ecc*cos(e0) );
-                    while (( abs(e1-e0) > small ) & ( ktr <= numiter ))
-                        ktr = ktr + 1;
-                        e0= e1;
-                        e1= e0 + ( m - e0 + ecc*sin(e0) ) / ( 1.0  - ecc*cos(e0) );
-                      end
-                    % -------------  find true anomaly  ---------------
-                    sinv= ( sqrt( 1.0 -ecc*ecc ) * sin(e1) ) / ( 1.0 -ecc*cos(e1) );
-                    cosv= ( cos(e1)-ecc ) / ( 1.0  - ecc*cos(e1) );
-                    nu  = atan2( sinv,cosv );
-                  else
-                    % -------------------- circular -------------------
+                }
+            }
+    }
+    ktr= 1;
+    e1 = e0 + ( (m-ecc*sinh(e0)+e0) / (ecc*cosh(e0) - 1.0 ) );
+    while ((abs(e1-e0)>small ) & ( ktr<=numiter ))
+        e0= e1;
+    e1= e0 + ( ( m - ecc*sinh(e0) + e0 ) / ( ecc*cosh(e0) - 1.0  ) );
+    ktr = ktr + 1;
+    end
+    // ----------------  find true anomaly  --------------------
+    sinv= -( sqrt( ecc*ecc-1.0  ) * sinh(e1) ) / ( 1.0  - ecc*cosh(e1) );
+    cosv= ( cosh(e1) - ecc ) / ( 1.0  - ecc*cosh(e1) );
+    nu  = atan2( sinv,cosv );
+    else
+        // --------------------- parabolic -------------------------
+        if ( abs( ecc-1.0  ) < small )
+            //%                c = [ 1.0/3.0; 0.0; 1.0; -m];
+            //%                [r1r] = roots (c);
+            //%                e0= r1r;
+            s = 0.5  * (halfpi - atan( 1.5 *m ) );
+    w = atan( tan( s )^(1.0 /3.0 ) );
+    e0= 2.0 *cot(2.0 *w);
+    ktr= 1;
+    nu = 2.0  * atan(e0);
+        else
+            // -------------------- elliptical ----------------------
+            if ( ecc > small )
+                // -----------  initial guess -------------
+                if ( ((m < 0.0 ) & (m > -pi)) | (m > pi) )
+                    e0= m - ecc;
+                else
+                    e0= m + ecc;
+    end
+    ktr= 1;
+    e1 = e0 + ( m - e0 + ecc*sin(e0) ) / ( 1.0  - ecc*cos(e0) );
+    while (( abs(e1-e0) > small ) & ( ktr <= numiter ))
+        ktr = ktr + 1;
+    e0= e1;
+    e1= e0 + ( m - e0 + ecc*sin(e0) ) / ( 1.0  - ecc*cos(e0) );
+    end
+    //-------------  find true anomaly  ---------------
+    sinv= ( sqrt( 1.0 -ecc*ecc ) * sin(e1) ) / ( 1.0 -ecc*cos(e1) );
+    cosv= ( cos(e1)-ecc ) / ( 1.0  - ecc*cos(e1) );
+    nu  = atan2( sinv,cosv );
+                else
+                    // -------------------- circular -------------------
                     ktr= 0;
-                    nu= m;
-                    e0= m;
-                  end
-              end
-          end
+    nu= m;
+    e0= m;
+    end
+    end
+    end
