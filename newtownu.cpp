80b13e906550d0c5788f0e69f607452e64fe3c9d
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-11-29 15:30:39 +0000

    Add files via upload

--- newtownu.cpp
+++ newtownu.cpp
@@ -65,49 +65,49 @@ if ( (ecc-1.0 ) > small )
 //% -------------------  initial guess -----------------------
    if ( ecc < 1.6  )
     {
-                if ( ((m<0.0 ) & (m>-pi)) | (m>pi) )
-                {
-                    e0= m - ecc;
-                }
-                else
-                {
-                    e0= m + ecc;
-                }
+        if ( ((m<0.0 ) & (m>-pi)) | (m>pi) )
+        {
+            e0= m - ecc;
+        }
+        else
+        {
+            e0= m + ecc;
+        }
+    }
+    else
+    {
+        if ( (ecc < 3.6 ) & (abs(m) > pi) )
+        {
+            if (m > 0)
+            {
+                e0= m - (m)*ecc;
             }
             else
             {
-                if ( (ecc < 3.6 ) & (abs(m) > pi) )
-                {
-                    if (m > 0)
-                    {
-                        e0= m - (m)*ecc;
-                    }
-                    else
-                    {
-                        e0= m + (m)*ecc;
-                    }
-                }
-                  else
-                  {
-                    e0= m/(ecc-1.0 );
-                  }
+                e0= m + (m)*ecc;
             }
-            ktr= 1;
-            e1 = e0 + ( (m-ecc*sinh(e0)+e0) / (ecc*cosh(e0) - 1.0 ) );
-            while ((abs(e1-e0)>small ) & ( ktr<=numiter ))
-            {
-                e0= e1;
-                e1= e0 + ( ( m - ecc*sinh(e0) + e0 ) / ( ecc*cosh(e0) - 1.0  ) );
-                ktr = ktr + 1;
-            }
-           // % ----------------  find true anomaly  --------------------
-            sinv = -( sqrt( ecc*ecc-1.0  ) * sinh(e1) ) / ( 1.0  - ecc*cosh(e1) );
-            cosv = ( cosh(e1) - ecc ) / ( 1.0  - ecc*cosh(e1) );
-            nu   = atan2( sinv,cosv );
-            }
-            else
-            {
-              //  % --------------------- parabolic -------------------------
+        }
+        else
+        {
+            e0= m/(ecc-1.0 );
+        }
+    }
+    ktr= 1;
+    e1 = e0 + ( (m-ecc*sinh(e0)+e0) / (ecc*cosh(e0) - 1.0 ) );
+    while ((abs(e1-e0)>small ) & ( ktr<=numiter ))
+    {
+        e0= e1;
+        e1= e0 + ( ( m - ecc*sinh(e0) + e0 ) / ( ecc*cosh(e0) - 1.0  ) );
+        ktr = ktr + 1;
+    }
+    // % ----------------  find true anomaly  --------------------
+    sinv = -( sqrt( ecc*ecc-1.0  ) * sinh(e1) ) / ( 1.0  - ecc*cosh(e1) );
+    cosv = ( cosh(e1) - ecc ) / ( 1.0  - ecc*cosh(e1) );
+    nu   = atan2( sinv,cosv );
+}
+else
+{
+            //  % --------------------- parabolic -------------------------
             if ( abs( ecc-1.0  ) < small )
             {
                  s = 0.5  * (halfpi - atan( 1.5 *m ) );
@@ -116,20 +116,20 @@ if ( (ecc-1.0 ) > small )
                 ktr= 1;
                 nu = 2.0  * atan(e0);
             }
-              else
-              {
-           //     % -------------------- elliptical ----------------------
+            else
+            {
+                //     % -------------------- elliptical ----------------------
                 if ( ecc > small )
                 {
-            //        % -----------  initial guess -------------
+                    //        % -----------  initial guess -------------
                     if ( ((m < 0.0 ) & (m > -pi)) | (m > pi) )
                     {
                         e0= m - ecc;
                     }
-                      else
-                      {
+                    else
+                    {
                         e0= m + ecc;
-                      }
+                    }
                     ktr= 1;
                     e1 = e0 + ( m - e0 + ecc*sin(e0) ) / ( 1.0  - ecc*cos(e0) );
                     while (( abs(e1-e0) > small ) & ( ktr <= numiter ))
@@ -138,19 +138,18 @@ if ( (ecc-1.0 ) > small )
                         e0= e1;
                         e1= e0 + ( m - e0 + ecc*sin(e0) ) / ( 1.0  - ecc*cos(e0) );
                     }
-            //        % -------------  find true anomaly  ---------------
+                    //        % -------------  find true anomaly  ---------------
                     sinv= ( sqrt( 1.0 -ecc*ecc ) * sin(e1) ) / ( 1.0 -ecc*cos(e1) );
                     cosv= ( cos(e1)-ecc ) / ( 1.0  - ecc*cos(e1) );
                     nu  = atan2( sinv,cosv );
                 }
-                  else
-                  {
-           //         % -------------------- circular -------------------
+                else
+                {
+                    //         % -------------------- circular -------------------
                     ktr= 0;
                     nu= m;
                     e0= m;
-                  }
                 }
-            
-  }
+            }
+}
 }
