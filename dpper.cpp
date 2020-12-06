c8f2d5397e2ae718ecb14df9f517b2f15bdcf497
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-12-06 14:29:43 +0000

    Add files via upload
    Updates.

--- dpper.cpp
+++ dpper.cpp
@@ -102,96 +102,22 @@ void dpper::set_parameters (double e3, double ee2, double peo,double pgho,double
    {
        m_zm = m_zmos;
    };
-   zf    = zm + 2.0 * zes * sin(zm);
-   sinzf = sin(zf);
-   f2    =  0.5 * sinzf * sinzf - 0.25;
-   f3    = -0.5 * sinzf * cos(zf);
-   ses   = se2* f2 + se3 * f3;
-   sis   = si2 * f2 + si3 * f3;
-   sls   = sl2 * f2 + sl3 * f3 + sl4 * sinzf;
-   sghs  = sgh2 * f2 + sgh3 * f3 + sgh4 * sinzf;
-   shs   = sh2 * f2 + sh3 * f3;
-   zm    = zmol + znl * t;
+   m_zf    = m_zm + 2.0 * m_zes * sin(m_zm);
+   m_sinzf = sin(m_zf);
+   m_f2    =  0.5 * m_sinzf * m_sinzf - 0.25;
+   m_f3    = -0.5 * m_sinzf * cos(m_zf);
+   m_ses   = m_se2 * m_f2 + m_se3 * m_f3;
+   m_sis   = m_si2 * m_f2 + m_si3 * m_f3;
+   m_sls   = m_sl2 * m_f2 + m_sl3 * m_f3 + m_sl4 * m_sinzf;
+   m_sghs  = m_sgh2 * m_f2 + m_sgh3 * m_f3 + m_sgh4 * m_sinzf;
+   m_shs   = m_sh2 * m_f2 + m_sh3 * m_f3;
+   m_zm    = m_zmol + m_znl * m_t;
+    
    if (init == 'y')
-       zm = zmol;
-   end
-   zf    = zm + 2.0 * zel * sin(zm);
-   sinzf = sin(zf);
-   f2    =  0.5 * sinzf * sinzf - 0.25;
-   f3    = -0.5 * sinzf * cos(zf);
-   sel   = ee2 * f2 + e3 * f3;
-   sil   = xi2 * f2 + xi3 * f3;
-   sll   = xl2 * f2 + xl3 * f3 + xl4 * sinzf;
-   sghl  = xgh2 * f2 + xgh3 * f3 + xgh4 * sinzf;
-   shll  = xh2 * f2 + xh3 * f3;
-   pe    = ses + sel;
-   pinc  = sis + sil;
-   pl    = sls + sll;
-   pgh   = sghs + sghl;
-   ph    = shs + shll;
-
-   if (init == 'n')
-       %   //  0.2 rad = 11.45916 deg
-       pe    = pe - peo;
-       pinc  = pinc - pinco;
-       pl    = pl - plo;
-       pgh   = pgh - pgho;
-       ph    = ph - pho;
-       inclp = inclp + pinc;
-       ep    = ep + pe;
-       sinip = sin(inclp);
-       cosip = cos(inclp);
-
-       % /* ----------------- apply periodics directly ------------ */
-       %   //  sgp4fix for lyddane choice
-       %   //  strn3 used original inclination - this is technically feasible
-       %   //  gsfc used perturbed inclination - also technically feasible
-       %   //  probably best to readjust the 0.2 limit value and limit discontinuity
-       %   //  use next line for original strn3 approach and original inclination
-       %   //  if (inclo >= 0.2)
-       %   //  use next line for gsfc version and perturbed inclination
-       if (inclp >= 0.2)
-           ph     = ph / sinip;
-           pgh    = pgh - cosip * ph;
-           argpp  = argpp + pgh;
-           nodep  = nodep + ph;
-           mp     = mp + pl;
-       else
-           % /* ---- apply periodics with lyddane modification ---- */
-           sinop  = sin(nodep);
-           cosop  = cos(nodep);
-           alfdp  = sinip * sinop;
-           betdp  = sinip * cosop;
-           dalf   =  ph * cosop + pinc * cosip * sinop;
-           dbet   = -ph * sinop + pinc * cosip * cosop;
-           alfdp  = alfdp + dalf;
-           betdp  = betdp + dbet;
-           nodep  = rem(nodep, twopi);
-           % sgp4fix for afspc written intrinsic functions
-           % nodep used without a trigonometric function ahead
-           if ((nodep < 0.0) & (opsmode == 'a'))
-               nodep = nodep + twopi;
-             end
-           xls    = mp + argpp + cosip * nodep;
-           dls    = pl + pgh - pinc * nodep * sinip;
-           xls    = xls + dls;
-           xnoh   = nodep;
-           nodep  = atan2(alfdp, betdp);
-           % sgp4fix for afspc written intrinsic functions
-           % nodep used without a trigonometric function ahead
-           if ((nodep < 0.0) & (opsmode == 'a'))
-               nodep = nodep + twopi;
-             end
-           if (abs(xnoh - nodep) > pi)
-               if (nodep < xnoh)
-                   nodep = nodep + twopi;
-               else
-                   nodep = nodep - twopi;
-               end
-           end
-           mp    = mp + pl;
-           argpp = xls - mp - cosip * nodep;
-       end
-   end % // if init == 'n'
+   {
+       m_zm = m_zmol;
+   };
+    
+   
 
 }
--- dpper.hpp
+++ dpper.hpp
@@ -43,10 +43,7 @@ private:
     xh3,    xi2,    xi3,    xl2,    xl3,    xl4,    zmol,
     zmos,   inclo,  init,   ep,     inclp,  nodep, argpp,  mp;
     
-    double m_twopi, m_pi, m_zns,   m_zm, m_zmos, m_zns, m_t;
-    m_zes,
-    m_znl,
-    m_zel;
+    double m_twopi, m_pi, m_zns, m_zm, m_zmos, m_t, m_zes, m_znl, m_zel, m_zf, m_sinzf, m_f2, m_f3, m_ses, m_sis, m_sls, m_sghs, m_shs, m_se2, m_si2, m_se3, m_si3, f3, m_sl2, m_sgh2, m_sh2, m_zmol, m_sl3, m_sgh3, m_sgh4, m_sl4, m_sh3;
     
     
 public:
@@ -57,7 +54,7 @@ public:
     
     void        set_parameters(orbital *model);
     void        set_parameters (double e3, double ee2, double peo,double pgho,double pho,double    pinco,double  plo,    double se2,double se3, double   sgh2, double  sgh3,  double sgh4, double  sh2, double   sh3,  double  si2,    double si3, double sl2, double   sl3,  double  sl4,  double  t, double     xgh2, double  xgh3, double  xgh4,   double xh2, double xh3,    double xi2,  double  xi3, double   xl2, double   xl3,   double xl4, double   zmol,
-    double zmos, double  inclo,  double init,  double  ep,  double   inclp,  double nodep, double argpp,  double mp);
+                                double zmos, double  inclo,  double init,  double  ep,  double   inclp,  double nodep, double argpp,  double mp);
     void        set_model();
     
 };
