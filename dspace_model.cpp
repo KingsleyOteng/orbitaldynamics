70e9802fdf27a99119ff235efb56cce4c367c1a6
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2020-12-17 07:30:07 +0000

    Add files via upload
    Updates.

--- dspace_model.cpp
+++ dspace_model.cpp
@@ -14,54 +14,55 @@ dspace_model::dspace_model()
 dspace_model::~dspace_model()
 {};
 
- dspace_model::dspace_model(double _d2201,double _d2211, double _d3210,
-double _d3222, double _d4410, double _d4422,
-double _d5220,double _d5232,double _d5421,
-double _d5433,double _dedt,double _del1,
-double _del2,double _del3,double _didt,
-double _dmdt,double _dnodt,double _domdt,
-double _irez,double _argpo,double _argpdot, double _t,
-double _tc, double _gsto,double _xfact,double _xlamo, double _no,
-double _atime, double _em, double _argpm, double _inclm, double _xli, double _mm,
-double _xni,double _nodem,double _nm)
-{
-    d2201 = _d2201;
-    d2211 = _d2211;
-    d3210 = _d3210;
-    d3222 = _d3222;
-    d4410 = _d4410;
-    d4422 = _d4422;
-    d5220 = _d5220;
-    d5232 = _d5232;
-    d5421 = _d5421;
-    d5433 = _d5433;
-    dedt = _dedt;
-    dedt = _dedt;
-    del2 = _del2;
-    del3 = _del3;
-    didt = _didt;
-    dmdt = _dmdt;
-    dnodt = _dnodt;
-    domdt = _domdt;
-    irez = _irez;
-    argpo = _argpo;
-    argpdot= _argpdot;
-    t = _t;
-    tc = _tc;
-    gsto = _gsto;
-    xfact = _xfact;
-    xlamo = _xlamo;
-    no = _no;
-    atime = _atime;
-    em = _em;
-    argpm = _argpm;
-    inclm = _inclm;
-    xli = _xli;
-    mm = _mm;
-    xni = _xni;
-    nodem = _nodem;
-    nm = _nm;
-};
+dspace_model::dspace_model(double _d2201,double _d2211, double _d3210,
+ double _d3222, double _d4410, double _d4422,
+ double _d5220,double _d5232,double _d5421,
+ double _d5433,double _dedt,double _del1,
+ double _del2,double _del3,double _didt,
+ double _dmdt,double _dnodt,double _domdt,
+ double _irez,double _argpo,double _argpdot, double _t,
+ double _tc, double _gsto,double _xfact,double _xlamo, double _no,
+ double _atime, double _em, double _argpm, double _inclm, double _xli, double _mm,
+ double _xni,double _nodem,double _nm)
+ {
+     d2201 = _d2201;
+     d2211 = _d2211;
+     d3210 = _d3210;
+     d3222 = _d3222;
+     d4410 = _d4410;
+     d4422 = _d4422;
+     d5220 = _d5220;
+     d5232 = _d5232;
+     d5421 = _d5421;
+     d5433 = _d5433;
+     dedt = _dedt;
+     dedt = _dedt;
+     del2 = _del2;
+     del3 = _del3;
+     didt = _didt;
+     dmdt = _dmdt;
+     dnodt = _dnodt;
+     domdt = _domdt;
+     irez = _irez;
+     argpo = _argpo;
+     argpdot= _argpdot;
+     t = _t;
+     tc = _tc;
+     gsto = _gsto;
+     xfact = _xfact;
+     xlamo = _xlamo;
+     no = _no;
+     atime = _atime;
+     em = _em;
+     argpm = _argpm;
+     inclm = _inclm;
+     xli = _xli;
+     mm = _mm;
+     xni = _xni;
+     nodem = _nodem;
+     nm = _nm;
+ };
+
 
 
 
@@ -97,7 +98,6 @@ void dspace_model::getModelParameters()
       ft    = 0.0;
     
     if (irez != 0)
-        
     {
         // sgp4fix streamline check
         if ((atime == 0.0) || (t * atime <= 0.0) || (abs(t) < abs(atime)) )
@@ -191,66 +191,63 @@ void dspace_model::getModelParameters()
             nm = no + dndt;
         };
     };
+    
 }
 
-
 void dspace_model::set_dspace_line1(double satrec_d2201, double satrec_d2211,double satrec_d3210, double satrec_d3222, double satrec_d4410, double satrec_d4422, double satrec_d5220, double satrec_d5232, double satrec_d5421)
-{
-     // single section of parameters for the dspace model
-    d2201 = satrec_d2201;
-    d2211 = satrec_d2211;
-    d3210 = satrec_d3210;
-    d3222 = satrec_d3222;
-    d4410 = satrec_d4410;
-    d4422 = satrec_d4422;
-    d5220 = satrec_d5220;
-    d5232 = satrec_d5232;
-    d5421 = satrec_d5421;
-};
+    {
+       //single section of parameters for the dspace model
+       d2201 = satrec_d2201;
+       d2211 = satrec_d2211;
+       d3210 = satrec_d3210;
+       d3222 = satrec_d3222;
+       d4410 = satrec_d4410;
+       d4422 = satrec_d4422;
+       d5220 = satrec_d5220;
+       d5232 = satrec_d5232;
+       d5421 = satrec_d5421;
+    };
 
-void dspace_model::set_dspace_line2(double satrec_d5433, double satrec_dedt, double satrec_del1,
-   double satrec_del2, double satrec_del3, double satrec_didt,
-   double satrec_dmdt, double satrec_dnodt, double satrec_domdt)
-{
-     // first section of parameters for the dspace model
-    d5433 = satrec_d5433;
-    dedt = satrec_dedt;
-    dedt = satrec_dedt;
-    del2 = satrec_del2;
-    del3 = satrec_del3;
-    didt = satrec_didt;
-    dmdt = satrec_dmdt;
-    dnodt = satrec_dnodt;
-    domdt = satrec_domdt;
-};
-   
-void dspace_model::set_dspace_line3(double satrec_irez,double satrec_argpo, double satrec_argpdot, double satrec_t, double satrec_tc, double satrec_gsto, double satrec_xfact, double satrec_xlamo, double satrec_no)
-{
-     // second section of parameters for the dspace model
-    irez = satrec_irez;
-    argpo = satrec_argpo;
-    argpdot= satrec_argpdot;
-    t = satrec_t;
-    tc = satrec_tc;
-    gsto = satrec_gsto;
-    xfact = satrec_xfact;
-    xlamo = satrec_xlamo;
-    no = satrec_no;
-    
-    
-};
-   
-void dspace_model::set_dspace_line4(double satrec_atime, double satrec_em, double satrec_argpm, double satrec_inclm, double satrec_xli, double satrec_mm, double satrec_xni,double satrec_nodem, double satrec_nm)
-{
-    // fourth section of parameters for the dspace model
-    atime = satrec_atime;
-    em = satrec_em;
-    argpm = satrec_argpm;
-    inclm = satrec_inclm;
-    xli = satrec_xli;
-    mm = satrec_mm;
-    xni = satrec_xni;
-    nodem = satrec_nodem;
-    nm = satrec_nm;
-    
-};
+   void dspace_model::set_dspace_line2(double satrec_d5433, double satrec_dedt, double satrec_del1,
+      double satrec_del2, double satrec_del3, double satrec_didt,
+      double satrec_dmdt, double satrec_dnodt, double satrec_domdt)
+   {
+        // first section of parameters for the dspace model
+       d5433 = satrec_d5433;
+       dedt = satrec_dedt;
+       dedt = satrec_dedt;
+       del2 = satrec_del2;
+       del3 = satrec_del3;
+       didt = satrec_didt;
+       dmdt = satrec_dmdt;
+       dnodt = satrec_dnodt;
+       domdt = satrec_domdt;
+   };
+      
+   void dspace_model::set_dspace_line3(double satrec_irez,double satrec_argpo, double satrec_argpdot, double satrec_t, double satrec_tc, double satrec_gsto, double satrec_xfact, double satrec_xlamo, double satrec_no)
+   {
+        // second section of parameters for the dspace model
+       irez = satrec_irez;
+       argpo = satrec_argpo;
+       argpdot= satrec_argpdot;
+       t = satrec_t;
+       tc = satrec_tc;
+       gsto = satrec_gsto;
+       xfact = satrec_xfact;
+       xlamo = satrec_xlamo;
+       no = satrec_no;
+   };
+      
+   void dspace_model::set_dspace_line4(double satrec_atime, double satrec_em, double satrec_argpm, double satrec_inclm, double satrec_xli, double satrec_mm, double satrec_xni,double satrec_nodem, double satrec_nm)
+   {
+       // fourth section of parameters for the dspace model
+       atime = satrec_atime;
+       em = satrec_em;
+       argpm = satrec_argpm;
+       inclm = satrec_inclm;
+       xli = satrec_xli;
+       mm = satrec_mm;
+       xni = satrec_xni;
+       nodem = satrec_nodem;
+       nm = satrec_nm;
+   };
