5d266b06d3fe19209a9606e83b2e608d269398e6
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2021-01-17 20:28:31 +0000

    Add files via upload
    Updates.

---
+++ mag.cpp
@@ -0,0 +1,28 @@
+//
+//  mag.cpp
+//  source
+//
+//  Created by Kwadwo Oteng-Amoako on 13/01/2021.
+//  Copyright © 2021 Kwadwo Oteng-Amoko. All rights reserved.
+//
+
+#include "mag.hpp"
+
+mag::mag()
+{
+    temp= m_vec_1*m_vec_1 + m_vec_2*m_vec_2 + m_vec_3*m_vec_3;
+
+    if (abs(temp) >= 1.0e-16)
+    {
+        m_mag=sqrt( temp );
+    }
+    else
+    {
+        m_xmag= 0.0;
+    }
+    
+    
+};
+
+mag::~mag()
+{};
---
+++ mag.hpp
@@ -0,0 +1,48 @@
+//
+//  mag.hpp
+//  source
+//
+//  Created by Kwadwo Oteng-Amoako on 13/01/2021.
+//  Copyright © 2021 Kwadwo Oteng-Amoko. All rights reserved.
+//
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <sstream>      // std::istringstream
+#include <iostream>     // std::cout
+#include <vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <time.h>
+#include <fstream>
+#include <stdio.h>
+#include <array>
+#include <iterator>
+#include <stdlib.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+#ifndef mag_hpp
+#define mag_hpp
+
+
+
+class mag
+{
+    
+    public:
+    mag();
+    ~mag();
+    double              m_vec_1, m_vec_2, m_vec_3, temp, m_mag;
+    
+    
+};
+
+#endif /* sdp4_hpp */
+
+
