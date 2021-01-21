3127cadaabec9714ed8958253582b1b4a343879d
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2021-01-21 23:10:13 +0000

    Add files via upload
    Updates.

---
+++ angl.cpp
@@ -0,0 +1,18 @@
+//
+//  angl.cpp
+//  source
+//
+//  Created by Kwadwo Oteng-Amoako on 21/01/2021.
+//  Copyright © 2021 Kwadwo Oteng-Amoko. All rights reserved.
+//
+
+#include "angl.hpp"
+
+angl::angl()
+{
+  
+
+};
+
+angl::~angl()
+{};
---
+++ angl.hpp
@@ -0,0 +1,51 @@
+//
+//  mag.hpp
+//  source
+//
+//  Created by Kwadwo Oteng-Amoako on 13/01/2021.
+//  Copyright © 2021 Kwadwo Oteng-Amoko. All rights reserved.
+//
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <sstream>      // std::istringstream
+#include <iostream>     // std::cout
+#include <vector>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <time.h>
+#include <fstream>
+#include <stdio.h>
+#include <array>
+#include <iterator>
+#include <stdlib.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+#ifndef angl_hpp
+#define angl_hpp
+
+
+
+class angl
+{
+    
+    private:
+        double m_angl;
+
+    
+    public:
+        angl();
+        ~angl();
+
+    
+};
+
+#endif /* sdp4_hpp */
+
+
