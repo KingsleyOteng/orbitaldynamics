31b3e7fc8a60b8ef168ade080f0a20c7dad33aba
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2021-03-24 21:50:26 +0000

    Add files via upload
    Updates.

--- flight-plot/main.cpp
+++ flight-plot/main.cpp
@@ -8,9 +8,11 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
 
-    pushButton addressBook;
-        addressBook.show();
+   // load the folder for accessing
+    //pushButton addressBook;
+      //  addressBook.show();
 
+    // show the main window
     MainWindow w;
     w.show();
     return a.exec();
