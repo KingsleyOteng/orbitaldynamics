da5866f3d8e6ef5c628197a5c9700023d4acbce0
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2021-03-24 21:50:12 +0000

    Add files via upload
    Updates.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
+
+   // load the folder for accessing
+    //pushButton addressBook;
+      //  addressBook.show();
+
+
+    // show the main window
     MainWindow w;
     w.show();
     return a.exec();
