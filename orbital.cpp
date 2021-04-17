e26c8a01da655c11f5ff6e6b8d98636b936541cd
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2021-04-17 09:20:33 +0000

    Add files via upload
    Updates for the qt gui implementation.

--- orbital.cpp
+++ orbital.cpp
@@ -655,6 +655,7 @@ std::string orbital::getWGS()
     return m_wgs;
 }
 
+// methods for QT gui development
 std::string orbital::getOrbit                        (){return ""; };
 double      orbital::getElevation                    (){return 0;};
 std::string orbital::getObjectName                   (){return "";};
