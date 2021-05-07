7630b3104f072fb49cb868ff4dcb64412bd750ba
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2021-05-07 13:00:34 +0000

    Add files via upload
    Updates to capturing user inputs.

--- main.cpp
+++ main.cpp
@@ -334,10 +334,15 @@ int main()
     // exit
     sgp4 *sgp_model = new sgp4();
     sgp_model -> set_parameters(orb = new orbital(12,13,14,"wgs-84"));
+    
+    if (user_input != "y")
+    {
     cout << "\n" <<" Please provide me the TLE number " << "\n";
     getline (cin, input_tle_number);
     cout << "\n" <<" Please confirm you input? " <<  "\n";
     getline (cin, user_input);
+    }
+    
     // check against the database if the number is correct
     // outproceed to next step some details about the TLE
     
