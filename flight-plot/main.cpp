1650a363debb0c054f8c78e4b0cb5b139c553208
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2021-05-10 13:41:46 +0000

    Add files via upload
    Updates - take user input.

--- flight-plot/main.cpp
+++ flight-plot/main.cpp
@@ -1,9 +1,16 @@
 #include "MainWindow.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <QApplication>
+#include <fstream>
+
+// declare the namespace
+using namespace std;
 
 int main(int argc, char *argv[])
 {
+    int temp;
+    getline (cin, temp);
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication a(argc, argv);
     MainWindow w;
