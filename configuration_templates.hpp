2d56a255cfc8fbbfc73d7ba0898301b01aec8842
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2021-06-08 14:40:54 +0000

    Add files via upload
    Updates.

--- configuration_templates.hpp
+++ configuration_templates.hpp
@@ -28,46 +28,12 @@ private:
    #define         JULIAN_DAY_OFFSET                       0.5
     
   // members
-  char             parse_input_phrase                       [ARRAY_SIZE];
-  char             parsed_string                            [ARRAY_SIZE];
-  double           m_epoch_julian_date;
-  double           m_epoch_julian_date_fraction;
-  int              m_epoch_julian_day;
-  int              m_epoch_julian_month;
-  int              m_epoch_julian_year;
-  int              m_epoch_gregorian_year;
-  int              m_epoch_gregorian_day_of_year;
-  int              m_epoch_greogorian_hour;
-  double           m_epoch_gregorian_minute;
-  double           m_epoch_gregorian_second;
-  double           m_epoch_gregorian_splits;
-  char             simple_date_buffer                       [ARRAY_LARGE_BUFFER];
-  char             return_string                            [ARRAY_EXTRA_LARGE_BUFFER];
-  struct           tm *ptr = new struct tm();
-
+ 
 public:
 
   // constructors  and destructors
-  utilities();
- ~utilities();
+  configuration_templates();
+  ~configuration_templates();
        
-
-  // getters and setters
-  // setters
-  void                      setStringParser                  (std::string str);
-  char*                     getStringParser                  (std::string str);
-  std::vector<std::string>  getStringParser                  ();
-
-    
-  // getters
-  void                      SetEpochJulianDate               (double epoch);
-  void                      SetSimpleDateFormatLong          ();
-
-  char*                     GetSimpleDateFormatLong          ();
-  double                    GetEpochJulianDate               ();
-  std::string               GetSatelliteLog                  (std::string phrasalmatching);
-  std::string               SatelliteNORADRecord             (std::string noradId, std::string indx);
-  void                      RungeKuttaMethod                 ();
-  void                      ParseDateFile                    (std::string fileName);
 };
 #endif /* coordinate_transforms_hpp */
