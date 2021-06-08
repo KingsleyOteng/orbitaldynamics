40b87a552e743a1a3d1806ed07b62813d191d9eb
Kwadwo Oteng-Amoako <49571112+KingsleyOteng@users.noreply.github.com>
2021-06-08 14:20:32 +0000

    Add files via upload
    Updates to the file.

--- configuration_templates.cpp
+++ configuration_templates.cpp
@@ -7,3 +7,43 @@
 //
 
 #include "configuration_templates.hpp"
+
+#endif /* configuration_templates_hpp */
+
+//
+//      Overview: Sets the codes defaults
+//
+//      Created by Kwadwo Oteng-Amoko on 08/Jun/2021.
+//      Copyright © 2021 Kwadwo Oteng-Amoko. All rights reserved.
+//
+#define Re                      6378.1370               // the earths radius; from Kelso
+#define we                      0.0000729211510        // Earth's rotation rate in radians/second; from Kelso
+
+// system files and preprocessor instructions
+#include <algorithm>                  // c-11 algoithm, vector libraries are used
+#include <array>
+#include <cmath>
+#include <fstream>
+#include <iterator>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <stdio.h>
+#include <time.h>
+#include <vector>
+#include <limits>
+#include <cctype>
+#include <iomanip>
+
+// third party apis
+#include <boost/any.hpp>               // boost standard library
+#include <boost/asio.hpp>              // io streaming headers
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/xpressive/xpressive.hpp>
+
+//#include <cppconn/driver.h> //#include <cppconn/exception.h> //#include <cppconn/prepared_statement.h>
+
+// declare the namespace
+using namespace std;
+using namespace boost::gregorian;
